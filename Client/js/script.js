5c441dbaa2f0b60be8b0436b6d74e1d2f6e33a2c
patloks12 <patloks12@gmail.com>
2018-10-01 16:47:30 +0200

    LibraryHelper v0.3 - Added Edit Books function

--- Client/js/script.js
+++ Client/js/script.js
@@ -25,10 +25,9 @@ $(function () {
     function GetAllBooks() {
         $.ajax({
             url: apiURL + "books/"
-        }).done(function (resp) {
-            functionRenderAllBooks(resp);
+        }).done(function (books) {
+            functionRenderAllBooks(books);
         }).fail(function (err) {
-
         })
     };
 
@@ -72,6 +71,7 @@ $(function () {
 
 
     $('#booksTable').on("click", "button.deleteBook", function () {
+        event.preventDefault();
         var bookID = $(this).closest("tr[data-book-id]").attr("data-book-id");
         RemoveBook(bookID);
     });
@@ -85,5 +85,86 @@ $(function () {
         }).fail(function (err) {
         });
     };
+
+    $('#booksTable').on("click", "button.editBook", function () {
+
+        var bookID = $(this).closest("tr[data-book-id]").attr("data-book-id");
+        var GlobalbookID = $("#you-will-never-find-me");
+        GlobalbookID.val(bookID);
+        console.log("kliknieto edit ksiazki o id " + bookID);
+        event.preventDefault();
+
+        addButton.html('<i class="fa fa-plus mr-2"></i>Edytuj');
+
+        GetBook(bookID);
+
+    });
+
+    function GetBook(bookID) {
+        $.ajax({
+            url: apiURL + "books/" + bookID
+        }).done(function (book) {
+            ShowBook(book);
+        }).fail(function (err) {
+        });
+    }
+
+    function ShowBook(book) {
+
+        var bookTitle = $("#form-title");
+        var bookAuthor = $("#form-author");
+
+        bookTitle.val(book.Title);
+        bookAuthor.val(book.Author);
+    }
+
+    function EditBook(bookID, currentBook) {
+        $.ajax({
+            url: apiURL + "books/" + bookID,
+            type: "PUT",
+            data: currentBook
+        }).done(function () {
+            GetAllBooks();
+            ResetBookInputs();
+        }).fail(function (err) {
+        });
+    }
+
+    var editButton = $("#form-add-button");
+
+    editButton.on("click", function (event) {
+        event.preventDefault();
+
+        var bookTitle = $("#form-title");
+        var bookAuthor = $("#form-author");
+
+        bookTitle.val();
+        bookAuthor.val();
+
+        var currentBook = {
+            Title: bookTitle.val(),
+            Author: bookAuthor.val()
+        }
+
+
+        if ($(this).text() == "Edytuj") {
+
+            var bookID = $("#you-will-never-find-me").eq(0).val();
+
+            EditBook(bookID, currentBook);
+
+            addButton.html('<i class="fa fa-plus mr-2"></i>Dodaj');
+        }
+    });
+
+    function ResetBookInputs(){
+
+        var bookTitle = $("#form-title");
+        var bookAuthor = $("#form-author");
+
+        bookTitle.val(" ");
+        bookAuthor.val(" ");
+    };
+
 
 });
