cfa124815e04a92ecab0eb7d55c8b6f4c4a39d60
patloks12 <patloks12@gmail.com>
2018-10-01 20:34:20 +0200

    LibraryHelper v0.4 - Added Readers functions (Add, Edit, Remove)

--- Client/js/script.js
+++ Client/js/script.js
@@ -157,7 +157,7 @@ $(function () {
         }
     });
 
-    function ResetBookInputs(){
+    function ResetBookInputs() {
 
         var bookTitle = $("#form-title");
         var bookAuthor = $("#form-author");
@@ -165,6 +165,175 @@ $(function () {
         bookTitle.val(" ");
         bookAuthor.val(" ");
     };
+
+
+    ///////////////////////////////////
+
+    GetAllReaders();
+
+    var addReaderButton = $("#reader-form-add-button");
+
+    addReaderButton.on("click", function (event) {
+        event.preventDefault();
+
+        var readerName = $("#reader-form-name").val();
+        var readerAge = $("#reader-form-age").val();
+
+        var newReader = {
+            Name: readerName,
+            Age: readerAge
+        }
+
+        if ($(this).text() == "Dodaj") {
+            AddNewReader(newReader);
+        }
+
+    });
+
+    function GetAllReaders() {
+        $.ajax({
+            url: apiURL + "readers"
+        }).done(function (readers) {
+            functionRenderAllReaders(readers);
+        }).fail(function (err) {
+        })
+    };
+
+    function AddNewReader(newReader) {
+        $.ajax({
+            url: apiURL + "readers",
+            type: "POST",
+            dataType: "json",
+            data: newReader
+        }).done(function () {
+            GetAllReaders();
+        }).fail(function (err) {
+        })
+    };
+
+    function functionRenderAllReaders(readers) {
+        var readersTable = $("#readersTable").find("tbody");
+
+        readersTable.html("");
+
+        for (var i = 0; i < readers.length; i++) {
+            var newRow = $("<tr data-reader-id=" + readers[i].ID + "></tr>");
+            var nameCol = $("<td>").text(readers[i].Name);
+            nameCol.appendTo(newRow);
+            var ageCol = $("<td>").text(readers[i].Age);
+            ageCol.appendTo(newRow);
+
+            var buttons = $(`        
+                <td>
+                    <div class="button-group">
+                        <button class="btn btn-primary btn-sm editReader">Edytuj</button>
+                        <button class="btn btn-danger btn-sm deleteReader">Usuń</button>
+                        <button class="btn btn-info btn-sm lendReader">Wypożycz</button>
+                    </div>
+                </td>)`);
+
+            newRow.append(buttons);
+            readersTable.append(newRow);
+        }
+    }
+
+
+    $('#readersTable').on("click", "button.deleteReader", function () {
+        event.preventDefault();
+        var readerID = $(this).closest("tr[data-reader-id]").attr("data-reader-id");
+        RemoveReader(readerID);
+    });
+
+    function RemoveReader(readerID) {
+        $.ajax({
+            url: apiURL + "readers/" + readerID,
+            type: "DELETE",
+        }).done(function () {
+            GetAllReaders();
+        }).fail(function (err) {
+        });
+    };
+
+    $('#readersTable').on("click", "button.editReader", function () {
+
+        var readerID = $(this).closest("tr[data-reader-id]").attr("data-reader-id");
+        var GlobalReaderID = $("#reader-you-will-never-find-me");
+        GlobalReaderID.val(readerID);
+        console.log("kliknieto edit czytelnika o id " + readerID);
+        event.preventDefault();
+
+        addReaderButton.html('<i class="fa fa-plus mr-2"></i>Edytuj');
+
+        GetReader(readerID);
+
+    });
+
+    function GetReader(readerID) {
+        $.ajax({
+            url: apiURL + "readers/" + readerID
+        }).done(function (reader) {
+            ShowReader(reader);
+        }).fail(function (err) {
+        });
+    }
+
+    function ShowReader(reader) {
+
+        var readerName = $("#reader-form-name");
+        var readerAge = $("#reader-form-age");
+
+        readerName.val(reader.Name);
+        readerAge.val(reader.Age);
+    }
+
+    function EditReader(readerID, currentReader) {
+        $.ajax({
+            url: apiURL + "readers/" + readerID,
+            type: "PUT",
+            data: currentReader
+        }).done(function () {
+            GetAllReaders();
+            ResetReaderInputs();
+        }).fail(function (err) {
+        });
+    }
+
+    var editReaderButton = $("#reader-form-add-button");
+
+    editReaderButton.on("click", function (event) {
+        event.preventDefault();
+
+        var readerName = $("#reader-form-name");
+        var readerAge = $("#reader-form-age");
+
+        readerName.val();
+        readerAge.val();
+
+        var currentReader = {
+            Name: readerName.val(),
+            Age: readerAge.val()
+        }
+
+
+        if ($(this).text() == "Edytuj") {
+
+            var readerID = $("#reader-you-will-never-find-me").eq(0).val();
+
+            EditReader(readerID, currentReader);
+
+            editReaderButton.html('<i class="fa fa-plus mr-2"></i>Dodaj');
+        }
+    });
+
+    function ResetReaderInputs() {
+
+        var readerName = $("#reader-form-name");
+        var readerAge = $("#reader-form-age");
+
+        readerName.val(" ");
+        readerAge.val(" ");
+    };
+
 
 
 });
